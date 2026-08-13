e5e4a7427f5206eff730d2ec1387b8ad55f5bf93
Bakka Nour <bakka_@msn.com>
2026-08-13 20:34:04 +0200

    Configure router basename for GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -7,17 +7,22 @@ import { InstallFilterPage } from './ui/pages/InstallFilterPage'
 import { CycleHistoryPage } from './ui/pages/CycleHistoryPage'
 import { StatisticsPage } from './ui/pages/StatisticsPage'
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <AppShell />,
+      children: [
+        { index: true, element: <Dashboard /> },
+        { path: 'objects/new', element: <ObjectFormPage /> },
+        { path: 'objects/:id', element: <ObjectDetailPage /> },
+        { path: 'objects/:id/install', element: <InstallFilterPage /> },
+        { path: 'objects/:id/history', element: <CycleHistoryPage /> },
+        { path: 'statistics', element: <StatisticsPage /> },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <AppShell />,
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: 'objects/new', element: <ObjectFormPage /> },
-      { path: 'objects/:id', element: <ObjectDetailPage /> },
-      { path: 'objects/:id/install', element: <InstallFilterPage /> },
-      { path: 'objects/:id/history', element: <CycleHistoryPage /> },
-      { path: 'statistics', element: <StatisticsPage /> },
-    ],
+    basename: '/filtrio',
   },
-])
+)
